2dffe754d2913d5f933a00597e3827cb6d4f42e9
mkhbo <mohit.kalra@warnermedia.com>
2022-02-28 15:18:45 -0800

    Update config_generator.py for validations
    Fixed validations in config_generator.py

--- config_generator.py
+++ config_generator.py
@@ -2,19 +2,22 @@
 import sys
 import time
 from random import randint
-from math import log, ceil
+from math import log, ceil, inf
 import hashlib
 from pprint import pprint
 import json
 
-def valid_int(i):
+def valid_int(i,s=0,e=inf):
     try:
         x = int(i)
-        return x > 0 
+        return x >= s and x <= e
     except:
         return False
 
 PRODUCT_CODE = 'hboMax'
+MAX_RECORDS_PER_USER = 1000 # assuming a binge user can only watch 1000 movies/episodes over 5 years
+MAX_NUM_DAYS = 365*5
+CONFIG_FILE_NAME = 'config.json'
 
 def get_rand_char():
     return chr(randint(97,122)) if randint(0,1) else chr(randint(65,90))
@@ -44,7 +47,7 @@ def get_user_array(num_users):
 # have atleast as many assets as the number of records
 # that need to be generated
 def get_asset_dict(num_assets):
-    # 70% features, 30% episode
+    # 70% features, 30% episodes
     num_features = ceil(0.7*num_assets)
     num_episodes = ceil(0.3*num_assets)
     features = [
@@ -65,10 +68,10 @@ def generate_config(num_users, duration, records_per_user):
     config['hurleyUserIds'] = get_user_array(num_users)
     config['assets'] = get_asset_dict(num_users*records_per_user)
     end_time = int(time.time())
-    config['updateTimeStart'] = end_time-duration
+    config['updateTimeStart'] = end_time-(duration*24*3600)
     config['updateTimeEnd'] = end_time
     config['recordsPerUser'] = records_per_user
-    with open('config.json', 'w', encoding='utf-8') as f:
+    with open(CONFIG_FILE_NAME, 'w', encoding='utf-8') as f:
         json.dump(config, f, ensure_ascii=False, indent=4)
 
 # python config_generator no_of_users no_of_days[OPTIONAL] records_per_user[OPTIONAL]
@@ -81,17 +84,19 @@ if __name__== "__main__":
     num_days = 90
     records_per_user = ceil(num_days/7) # 1 movie per week
 
-    if not valid_int(num_users):
-        sys.exit('please provide a positive int for number of users')
+    if not valid_int(num_users,1,500):
+        sys.exit('number of users can be between 1 and 1000')
     
-    if len(sys.argv)==3:
+    if len(sys.argv)>=3:
         num_days = sys.argv[2]
-        if not valid_int(num_days):
-            sys.exit('please provide a positive int for number of days')
+        if not valid_int(num_days,1,MAX_NUM_DAYS):
+            sys.exit('number of days should be between 1 and 1825(rougly 5 years)')
 
-    if len(sys.argv)==4:
+    if len(sys.argv)>=4:
         records_per_user = sys.argv[3]
-        if not valid_int(records_per_user):
-            sys.exit('please provide a positive int for number of records per user')
-
+        if not valid_int(records_per_user,1, MAX_RECORDS_PER_USER):
+            sys.exit('number of records per user can range between 1 and 1000(watched roughly for 5 years)')
+    
+    start_time = int(time.time())
     generate_config(int(num_users), int(num_days), int(records_per_user))
+    print('generated config for {0} users. Time taken : {1}s'.format(num_users, str(round(time.time()-start_time,2))))
